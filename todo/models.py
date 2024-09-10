ca441300472a3a631ebeacd6f8b412ecf637d027
dhiraj-desai <131889248+dhiraj-desai@users.noreply.github.com>
2024-09-10 23:51:14 +0530

    Update models.py

--- todo/models.py
+++ todo/models.py
@@ -14,10 +14,4 @@ class Todo(models.Model):
         return self.task
 
 
-# class TodoItem(models.Model):
-#     title = models.CharField(max_length=200)
-#     completed = models.BooleanField(default=False)
-#     created_at = models.DateTimeField(auto_now_add=True)
 
-#     def __str__(self):
-#         return self.title
