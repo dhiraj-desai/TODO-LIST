1b128e419128d08f7f557df15a13334223ab846c
dhiraj-desai <131889248+dhiraj-desai@users.noreply.github.com>
2024-09-10 23:52:18 +0530

    Update views.py

--- todo/views.py
+++ todo/views.py
@@ -85,15 +85,7 @@ def protected_view(request):
     return Response({"message": "This is a protected view."})
 
 
-# @api_view(["POST"])
-# def register(request):
-#     serializer = RegisterSerializer(data=request.data)
-#     if serializer.is_valid():
-#         serializer.save()
-#         return Response(
-#             {"message": "User created successfully."}, status=status.HTTP_201_CREATED
-#         )
-#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 @api_view(["POST"])
@@ -117,97 +109,3 @@ def login(request):
         )
 
 
-# def todo_list(request):
-#     todos = TodoItem.objects.all()  # Retrieve all to-do items
-
-#     if request.method == 'POST':
-#         if 'todo_id' in request.POST:  # Check if we're updating an existing item
-#             todo_id = request.POST.get('todo_id')
-#             todo = get_object_or_404(TodoItem, id=todo_id)
-#             form = TodoForm(request.POST, instance=todo)
-#         else:  # Adding a new item
-#             form = TodoForm(request.POST)
-
-#         if form.is_valid():
-#             form.save()  # Save the data (either new or updated)
-#             return redirect('todo_list')  # Redirect to the same page to display updated data
-
-#     else:
-#         form = TodoForm()  # Provide an empty form for adding new items
-
-#     return render(request, 'todo/todo_list.html', {'todos': todos, 'form': form})
-
-# def delete_todo(request, todo_id):
-#     todo = get_object_or_404(TodoItem, id=todo_id)
-#     todo.delete()
-#     return redirect(todo_list)
-
-
-# def item_list(request):
-#     if request.method == 'POST':
-#         form = ItemForm(request.POST)
-#         if form.is_valid():
-#             form.save()  # Save the form data to the database
-#             return redirect(item_list)  # Redirect to the same view to display the updated list
-#     else:
-#         form = ItemForm()
-
-#     items = Item.objects.all()  # Fetch all items from the database
-#     return render(request, 'todo/items_list.html', {'form': form, 'items': items})
-
-#     # if 'my_list' not in request.session:
-#     #     request.session['my_list'] = []
-
-
-#     # if request.method == 'POST':
-
-#     #     new_item = request.POST.get('item')
-#     #     if new_item:
-#     #         request.session['my_list'].append(new_item)
-#     #         request.session.modified = True
-
-#     #     return redirect(form_list_view)
-
-#     # # Pass the updated list to the template for display
-#     # return render(request, 'todo/index.html', {'my_list': request.session['my_list']})
-
-# def tlist(request):
-#     pass
-#     # return render(request, "todo/index.html")
-
-
-# def update_item_view(request):
-
-#     if request.method == 'POST':
-
-#         item_index = int(request.POST.get('item_index'))
-#         updated_item = request.POST.get('updated_item')
-#         form = ItemForm(request.POST)
-#         if form.is_valid():
-#             form.save()  # Save the form data to the database
-#             return redirect(item_list)  # Redirect to the same view to display the updated list
-#         else:
-#             form = ItemForm()
-
-#         items = Item.objects.all()  # Fetch all items from the database
-#         return render(request, 'todo/items_list.html', {'form': form, 'items': items})
-
-#     #     if 'my_list' in request.session and updated_item:
-#     #         my_list = request.session['my_list']
-#     #         if 0 <= item_index < len(my_list):
-#     #             my_list[item_index] = updated_item  # Update the item in the list
-#     #             request.session.modified = True
-#     #     return redirect(form_list_view)
-
-
-# def delete_item_view(request):
-#     pass
-#     # if request.method == 'POST':
-#     #     item_index = int(request.POST.get('item_index'))
-#     #     # Remove the item from the list using its index
-#     #     if 'my_list' in request.session:
-#     #         my_list = request.session['my_list']
-#     #         if 0 <= item_index < len(my_list):
-#     #             del my_list[item_index]
-#     #             request.session.modified = True
-#     #     return redirect(form_list_view)
